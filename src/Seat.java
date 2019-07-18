c056db209f7235c688ee70f8761f4673d2c2bfcc
Alice Storey <36473318+Alice-Storey@users.noreply.github.com>
2019-07-18 08:33:40 -0500

    data members for Seat

--- src/Seat.java
+++ src/Seat.java
@@ -1,4 +1,13 @@
 
 public class Seat {
-
+	enum Row {
+		A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z
+	}
+	
+	private String initials,
+	clientid;
+	private boolean canBeAssigned;
+	private Row r;
+	private int c;
+	
 }
