c1a2eba25c8cf86f2a5b7f1dd3893c343b9cd6cf
Alice Storey <36473318+Alice-Storey@users.noreply.github.com>
2019-07-18 11:08:44 -0500

    Room methods: assign a client, get sizes

--- src/Room.java
+++ src/Room.java
@@ -1,8 +1,29 @@
-
 public class Room {
-	private Seat[][] seats;
+	private Seat[][] seats; //row, col
 	
 	public Room(int rows, int cols) {
 		seats = new Seat[rows][cols];
+	}
+	
+	public int getNumRows() {
+		return seats.length;
+	}
+	
+	public int getNumCols() {
+		return seats.length>0 ? seats[0].length : 0;
+	}
+	
+	public void assign(int r, int c, Client client) {
+		if (r<getNumRows() && c<getNumCols()) {
+			seats[r][c].setClient(client);
+		}
+	}
+	
+	public void assign(char r, int c, Client client) {
+		assign(Seat.rowNum(r), c, client);
+	}
+	
+	public void assign(Seat.Row r, int c, Client client) {
+		assign(r.ordinal(), c, client);
 	}
 }
