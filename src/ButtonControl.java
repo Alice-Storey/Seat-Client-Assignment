cf7fa6512ecf31dbbaf33cfcf48ec198a943f961
Alice Storey <36473318+Alice-Storey@users.noreply.github.com>
2019-07-30 14:44:04 -0500

    seat grid vgap

--- src/ButtonControl.java
+++ src/ButtonControl.java
@@ -17,7 +17,7 @@ public class ButtonControl implements ActionListener {
 		int rows = room.getNumRows(),
 				cols = room.getNumCols();
 		
-		panel.setLayout(new GridLayout(rows+1, cols+1) );
+		panel.setLayout(new GridLayout(rows+1, cols+1, 0, 8) );
 		panel.setSize(rows*60, cols*60);
 //		panel.setBorder(BorderFactory.createEmptyBorder(2,2,2,2));
 		panel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
